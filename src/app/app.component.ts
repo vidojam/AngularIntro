ec6dab073e1205d1f8f0caaa834a1d580f05db74
Jose Torres <vidojam@gmail.com>
2023-08-27 21:38:12 -0400

    keyup & .KeyCode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,8 +14,15 @@ export class AppComponent {
 
   // event binding in angular
   buttonClick () {
-    console.log('Button Click Event worked')
+    console.log('Button Click Event worked');
   }
+
+  onKeyup($event: any) {
+    // console.log('Keyup event working');
+    console.log($event.keyCode);
+  }
+
+
 
   // message: string = 'Message From Typescript Component File'
   // imgUrl: string = 'https://plus.unsplash.com/premium_photo-1669497495583-d552beb8ece4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1462&q=80';
