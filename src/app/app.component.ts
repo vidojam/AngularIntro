23cd98704a6964308dca169c6f417db7658f000a
Jose Torres <vidojam@gmail.com>
2023-08-29 17:01:36 -0400

    one/two way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, ViewChild, AfterViewInit } from '@angular/core';
-import { PostComponent } from './post/post.component';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -9,71 +9,20 @@ import { PostComponent } from './post/post.component';
 
 
 export class AppComponent {
+  postTitle: string = "";
+  postDetails: string = "";
+  imageURL: string = "";
+  postURL: string ="";
+  addBackground: boolean= true;
 
-  bool:boolean = true;
-
-  // event binding in angular
-  buttonClick () {
-    console.log('Button Click Event worked');
-  }
-
-  // console.log('Keyup event working');
-  // console.log($event.keyCode);
-
-  // Event filtering - gets Enter key pressed in inspector
-  // onKeyup() {
-  //     console.log('Enter key pressed');
-  // }
-
-  // How we get the value in the text box
-  // onKeyup($event: any) {
-  //     console.log($event.target.value);
-  // }
-
-  // Get variable username is used to capture the parameter
-  onKeyup(username: any) {
-      console.log(username);
-  }
-
-  
-
-
-  // onKeyup($event: any) {
-  //   // console.log('Keyup event working');
-  //   // console.log($event.keyCode);
+ 
+  constructor() {  
+   
     
-  //   if($event.keyCode == 13 ){
-  //     console.log('Enter key pressed');
-  //   }
-  // }
 
-
-
-  // message: string = 'Message From Typescript Component File'
-  // imgUrl: string = 'https://plus.unsplash.com/premium_photo-1669497495583-d552beb8ece4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1462&q=80';
-
-
-// export class AppComponent implements AfterViewInit {
-  // title = 'AngularIntro';
-  // parentMessage: string = 'Message changed';
-  // // message!:string;
-  // fromChildOutput!:string;
-  
-
-
-  // @ViewChild(PostComponent) childComp: PostComponent | undefined;
-
-  constructor() {
-    // No need to initialize message here anymore
+    
   }
 
-//   ngAfterViewInit() {
-//     console.log(this.childComp);
-//     if (this.childComp) {
-//       this.message = this.childComp.childMessage;
-//     }
-//   }
-//   reciveMessage($event: any) {
-//     this.fromChildOutput = $event;
-//   }
+
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,12 +8,14 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { PostComponent } from './post/post.component';
 import { PostListComponent } from './post-list/post-list.component';
 
+
 @NgModule({
   declarations: [
     AppComponent, NavbarComponent, PostComponent, PostListComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
