f98c29ca7fcc1e978f9e04984e808e791735ac9b
Jose Torres <vidojam@gmail.com>
2023-09-05 18:38:33 -0400

    directive - ngIf & abjArray

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,38 +11,35 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   postArray: Array<string> = ['Post 1', 'Post 2', 'Post 3', 'Post 4', 'Post 5'];
+  post: any; // Define 'post' property with the appropriate type
+  postTitle: any;
 
-  // objArray: Array<object> = [
+  objArray: Array<any> = []
+
+
+
+  // for postTitle only, must be... any
+  // objArray: Array<any> = [
   //   { id: 1, postTitle: 'Post 1' },
+  //   { id: 2, postTitle: 'Post 2' },
   //   { id: 3, postTitle: 'Post 3' },
   //   { id: 4, postTitle: 'Post 4' },
   //   { id: 5, postTitle: 'Post 5' },
   // ]
 
 
-
-  // for postTitle only, must be... any
-  objArray: Array<any> = [
-    { id: 1, postTitle: 'Post 1' },
-    { id: 2, postTitle: 'Post 2' },
-    { id: 3, postTitle: 'Post 3' },
-    { id: 4, postTitle: 'Post 4' },
-    { id: 5, postTitle: 'Post 5' },
-  ]
-
-
   
   constructor() {  
-    for (let i = 0; i < this.postArray.length; i++)
-      console.log(this.postArray[i])
+    // for (let i = 0; i < this.postArray.length; i++)
+    //   console.log(this.postArray[i])
   }
 
   addNew() {
     this.objArray.push({ id: 6, postTitle: 'Post 6'});
   }
 
-  onDelete(post: any) {
-    let index = this.objArray.indexOf(post);
+  onDelete(index: any) {
+    this.objArray.indexOf(this.post);
     this.objArray.splice(index, 1)
   }
 
